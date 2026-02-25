24eac5d5df2f20205b3d613e940f167009d18648
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-02-25 14:17:56 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,9 @@
+function submitForm() {
+	const nume=document.getElementById("name").value;
+	const email=document.getElementById("email").value;
+	const message=document.getElementById("message").value;
+	console.log(nume);
+	console.log(email);
+	console.log(message);
+	console.warn("Goodbye World!");
+}
