0e3d5df979afadb338ca50c31366931003b73a9b
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-02-25 15:06:13 +0200

    Adaugare greet in functie de timpul zilei

--- js/contact_form.js
+++ js/contact_form.js
@@ -2,8 +2,24 @@ function submitForm() {
 	const nume=document.getElementById("name").value;
 	const email=document.getElementById("email").value;
 	const message=document.getElementById("message").value;
+
+
 	console.log(nume);
 	console.log(email);
 	console.log(message);
 	console.warn("Goodbye World!");
+}
+
+const ora=new Date().getHours();
+const text=document.querySelector("header p");
+if (ora >= 6 && ora <= 11)
+{
+	text.textContent ="Buna dimineata! Bine ai venit pe pagina mea.";
+}
+else if (ora >= 12 && ora <= 17)
+{
+	text.textContent ="Buna ziua! Bine ai venit pe pagina mea.";
+}
+else {
+	text.textContent ="Buna seara! Bine ai venit pe pagina mea.";
 }
