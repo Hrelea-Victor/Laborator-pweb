282553c47c68bef9f78d7ba93da85b4491b1a49a
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-04-15 15:04:58 +0300

    Feat: About page(momentan goala)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
+import About from './pages/About';
 import Navbar from './Navbar';
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/contact" element={<Contact />} />
+				<Route path="/about" element={<About />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
