72bd9cf595eb9872da0d08962fa7b9cf5f8fd6ce
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-04-15 14:34:56 +0300

    Feat: Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Navbar from './Navbar'
 function App() {
 	return (
 		<BrowserRouter>
-		<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/projects" element={<Projects />} />
-		<Route path="/contact" element={<Contact />} />
-		</Routes>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
 		</BrowserRouter>
 	);
 }
