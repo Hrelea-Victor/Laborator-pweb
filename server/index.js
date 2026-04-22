58f0356257302cd396149c56092391375b6ab625
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-04-22 12:13:22 +0300

    Feat: Rute specifice

--- server/index.js
+++ server/index.js
@@ -3,20 +3,37 @@ const app = express();
 const PORT = 3000;
 // Prima ruta: raspunde la GET /
 app.get('/', function(req, res) {
-res.json({ message: 'Serverul functioneaza!' });
+	res.json({ message: 'Serverul functioneaza!' });
 });
 // Date (temporar in memorie, vom folosi MongoDB mai tarziu)
 const projects = [
-{ id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
-{ id: 2, title: "Calculator Buget", tech: "JS", done: true },
-{ id: 3, title: "Dashboard React", tech: "React", done: false },
-{ id: 4, title: "API Meteo", tech: "React, API", done: false },
+	{ id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
+	{ id: 2, title: "Calculator Buget", tech: "JS", done: true },
+	{ id: 3, title: "Dashboard React", tech: "React", done: false },
+	{ id: 4, title: "API Meteo", tech: "React, API", done: false },
 ];
 // GET /api/projects - returneaza toate proiectele
 app.get('/api/projects', function(req, res) {
-res.json(projects);
+	res.json(projects);
 });
+// GET pt proiecte individuale
+app.get('/api/projects/:id', function(req, res) {
+	if(projects.find(p => p.id === parseInt(req.params.id)))
+		res.json(projects.find(p => p.id === parseInt(req.params.id)));
+	else
+		res.status(404).json({error: 'Not found' });
+});
+
+app.get('/api/stats', function(req, res) {
+	const result = {
+		"total": projects.length,
+		"done": projects.filter(p => p.done).length,
+		"in work" : projects.filter(p => !p.done).length,
+	};
+	res.json(result);
+});
+
 // Porneste serverul
 app.listen(PORT, function() {
-console.log('Server pornit pe http://localhost:' + PORT);
+	console.log('Server pornit pe http://localhost:' + PORT);
 });
