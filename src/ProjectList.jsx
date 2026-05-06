a01c39974619c7f02a9552e759e7d1f34af6ec7a
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-05-06 15:47:23 +0300

    Feat: delete button pentru fiecare proiect

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -42,14 +42,31 @@ setTech('');
 console.error('Eroare:', err);
 }
 }
+async function handleDelete(id) {
+	try {
+		const response = await fetch('http://localhost:3000/api/projects/' + id,{
+			method: 'DELETE',
+		});
+		setProjects(projects.filter(p => p._id !== id))
+	} catch (err) {
+		console.error('Eroare:' , err);
+	}
+}
 return (
 <div>
 <h3>Proiecte</h3>
 <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+<ul>
 {projects.filter(function(p) { 
 return p.title.toLowerCase().includes(filter.toLowerCase()); }).map(function(item, index) {
-return <Card key={item.id} title={item.title} description={item.tech} done={item.done}/>;
+return (
+	<li key={item.id}>
+		<Card title={item.title} description={item.tech} done={item.done}/>
+		<button onClick={() => handleDelete(item._id)}>Delete</button>
+	</li>
+);
 })}
+</ul>
 <div>
 <p>Proiecte totale: {projects.length}</p>
 <p>Proiecte finalizate: {projects.filter(p => p.done).length}</p>
