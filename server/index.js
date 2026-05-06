967a0a00e99fc0913e89dbd3829696bcccdc49d5
Victor Hrelea <victor.hrelea@student.unitbv.ro>
2026-05-06 14:33:47 +0300

    Feat: rescris POST pt DB

--- server/index.js
+++ server/index.js
@@ -46,16 +46,18 @@ app.get('/api/stats', function(req, res) {
 });
 */
 // POST /api/projects - adauga un proiect nou
-app.post('/api/projects', function(req, res) {
-	 const newProject = {
-		"id": projects.length + 1,
-		"title": req.body.title,
-		"tech": req.body.tech,
-		"done": req.body.done || false,
-	};
-	console.log(newProject); 
-	projects.push(newProject);
-	res.status(201).json(newProject);
+app.post('/api/projects', async function(req, res) {
+	try { 
+		const newProject = new Project({
+			title: req.body.title,
+			tech: req.body.tech,
+			done: req.body.done || false,
+		});
+		const saved = await newProject.save();
+		res.status(201).json(saved);
+	} catch (err) {
+		res.status(400).json({ error: err.message});
+	}
 });
 
 //delete
